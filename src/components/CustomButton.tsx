af30db7a954363cc009ba2e7562c80b0914d9a8a
Sheila Alamo <sheilaalamoalarcon@gmail.com>
2024-11-02 16:13:07 +0000

    refactor: add anchor element to button for navigation between pages

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,22 +1,18 @@
+import type { WebRoutesEnum } from "@/constants/enums";
+import styles from "../styles/CButton.module.css";
+
 interface Props {
   title: string;
+  href?: WebRoutesEnum;
 }
-export default function CustomButton({ title }: Props) {
+export default function CustomButton({ title, href }: Props) {
   return (
-    <button
-      id="custom_button"
-      style={
-        "display: flex;height: 2.625rem;padding: 0.5625rem 7rem;justify-content: center;align-items: center;gap: 0.625rem;flex-shrink: 0;border-radius: var(--Corner-Extra-small, 0.25rem);background: var(--TEXT, #45362f);"
-      }
-    >
-      <p
-        id="custom_button_title"
-        style={
-          "  color: var(--SECONDARY, #fede71);font-size: 1.125rem;font-style: normal;font-weight: 400;line-height: normal;"
-        }
-      >
-        {title}
-      </p>
-    </button>
+    <a href={href}>
+      <button id="custom-button" className={styles.container}>
+        <p id="custom-button-title" className={styles.title}>
+          {title}
+        </p>
+      </button>
+    </a>
   );
 }
