b5608e9312cee1a7ab84cf2f18e51e40f8801d97
Sheila Alamo <sheilaalamoalarcon@gmail.com>
2024-11-02 16:11:42 +0000

    refactor: add auth layout to login add reactive change to custom input for different input types

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,24 +1,66 @@
+import type { HTMLInputTypeAttribute } from "preact/compat";
+import { useLayoutEffect, useState } from "preact/hooks";
+import styles from "../styles/CInput.module.css";
 interface CustomInputProps {
   name: string;
   placeholder: string;
+  type: HTMLInputTypeAttribute;
+  required: boolean;
 }
-export default function CustomInput({ name, placeholder }: CustomInputProps) {
-  const styles: string[] = [
-    "display: flex;width: 20.75rem;flex-direction: column;justify-content: center;align-items: flex-start;gap: 0.3125rem;",
-    "color: var(--PRIMARY, #f36337);text-align: center;/* INPUT LABEL BOLD */font-family: Urbanist; font-size: 0.875rem;font-style: normal;font-weight: 700;line-height: normal;text-transform:capitalize",
-    "display: flex;flex-direction: column;justify-content: center;align-items: flex-start;align-self: stretch;width: 20.75rem;height: 2.5rem;gap: 0.625rem;padding: 0.5625rem 0.625rem;border-radius: 0.3125rem;border: 1px solid var(--Disabled, #B3B3B3);",
-  ];
+export default function CustomInput({
+  name,
+  placeholder,
+  type,
+  required = false,
+}: CustomInputProps) {
+  const [props, setProps] = useState({});
+
+  function switchProps() {
+    const basicProps = {
+      type,
+      required,
+      placeholder,
+      name,
+    };
+    switch (type) {
+      case "file":
+        return setProps({
+          ...basicProps,
+          accept: "image/png",
+          className: styles.inputFile,
+        });
+      case "password":
+        return setProps({
+          ...basicProps,
+          minlength: "8",
+          className: styles.input,
+        });
+      case "email":
+        return setProps({
+          ...basicProps,
+          pattern: ".+@example.com",
+          className: styles.input,
+        });
+      case "text":
+        return setProps({
+          ...basicProps,
+          minlength: "8",
+          maxlength: "25",
+          className: styles.input,
+        });
+      default:
+        break;
+    }
+  }
+  useLayoutEffect(() => {
+    switchProps();
+  }, [type]);
   return (
-    <div style={styles[0]} id={"input-container"}>
-      <p style={styles[1]} id="input-title">
+    <div className={styles.customInput} id={"input-container"}>
+      <p className={styles.title} id="input-title">
         {name}
       </p>
-      <input
-        style={styles[2]}
-        id="input"
-        name={name}
-        placeholder={placeholder}
-      />
+      <input class={"custom-input"} id="input" {...props} />
     </div>
   );
 }
