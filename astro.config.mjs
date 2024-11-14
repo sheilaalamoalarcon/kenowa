7ad7ccdc492268b8c89da7b40d631939f4a38a1b
Sheila Alamo <sheilaalamoalarcon@gmail.com>
2024-11-14 15:57:34 +0000

    feat: add SSR config and change constants

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  output: 'hybrid',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,12 +6,9 @@ import {
   ImagesSourceEnum,
   type CastleImage,
 } from "@/components/LandingImages.astro";
+import { atom } from "nanostores";
 
 export const defaultBttns: ICustomButton[] = [
-  {
-    title: "Test Prototype",
-    href: WebRoutesEnum.LOG_IN,
-  },
   {
     title: "Log In",
     href: WebRoutesEnum.LOG_IN,
@@ -43,10 +40,6 @@ export const userBttns: ICustomButton[] = [
   {
     title: "bag",
     href: WebRoutesEnum.PAYING_HALL,
-  },
-  {
-    title: "Store",
-    href: WebRoutesEnum.SHOP,
   },
   {
     title: "Log out",
@@ -112,23 +105,23 @@ export const produtDescription: string =
   "Preparate para sumergirte en nuestra web y encontrar los productos con las mejores reviews solo de clientes 100% reales. Share all your visions in the global chat. Search products with honest reviews. Build your fashion online store.";
 export const landingCopies: CParagraph[] = [
   {
-    title: "Share all your visions in the global chat.",
+    title: "Share All Your Visions: Connect, Share, and Collaborate",
     description:
-      "Comparte tus ideas y opiniones con el resto de la comunidad en nuestro chat global.",
+      "Join the conversation in our Global Chat! This space is designed for you to connect with the entire community, exchange ideas, get feedback, and spark new discussions. It’s a real-time feature where you can share your thoughts and explore new perspectives with like-minded users. Dive in, start a chat, and let your voice be part of the collective conversation. This is where ideas come to life — together.",
   },
   {
-    title: "Search products with honest reviews.",
+    title: "Search Honest Reviews: Find Genuine Insights",
     description:
-      "Preparate para sumergirte en nuestra web y encontrar los productos con las mejores reviews solo de clientes 100% reales.",
+      "Discover authentic feedback. Skip the noise and get straight to the real experiences from users like you. Whether you're looking for opinions on products, services, or experiences, our platform prioritizes transparency, helping you make informed decisions with trustworthy, unbiased reviews. Find what matters most with confidence.",
   },
   {
-    title: "Build your online store.",
+    title: "Get Style Inspiration: Discover Your Next Look",
     description:
-      "Crea tu propia tienda online de moda y empieza a vender tus productos en todo el mundo.",
+      "Unleash your creativity. Browse a curated selection of looks, ideas, and trends from real users and fashion enthusiasts. Whether you're updating your wardrobe or looking for fresh styling tips, this is your go-to space for discovering the latest trends and finding inspiration tailored to your taste. Start exploring and elevate your style game today!",
   },
 ];
 export const welcomeMessage: string =
-  "Bienvenido a Kenowa. Preparate para sumergirte en nuestra web y encontrar los productos con las mejores reviews solo de clientes 100% reales. Share all your visions in the global chat. Search products with honest reviews. Build your fashion online store.";
+  "Welcome to your go-to space for genuine insights and creative inspiration. Explore trusted reviews, share your own experiences, and find fresh style ideas, all in one place. Dive into our global chat to connect with a vibrant community, exchange opinions, and spark new conversations. Whether you're searching for the best recommendations or looking to inspire others, this is where real voices meet innovative ideas. Join the conversation and start exploring now!";
 
 export const landingImages: CastleImage[] = [
   {
@@ -148,3 +141,5 @@ export const landingImages: CastleImage[] = [
     isRound: false,
   },
 ];
+export const $isOpen = atom<boolean>(false);
+export const $isLogged = atom<boolean>(false);
