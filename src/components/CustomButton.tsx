f7a097e04fa402f22808bb767ae85c0c5a17d1bc
Sheila Alamo <sheilaalamoalarcon@gmail.com>
2024-11-14 16:05:25 +0000

    refactor: change custom button and other files where there is cButton

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,19 +1,41 @@
-import type { WebRoutesEnum } from "@/constants/enums";
+import { useStore } from "@nanostores/preact";
 import styles from "../styles/CButton.module.css";
+import { $isOpen } from "@/constants/constants";
+import { ClickActions } from "@/constants/enums";
 
 interface Props {
   title: string;
-  href?: WebRoutesEnum;
+  clickAction: ClickActions;
   style?: string;
+  type?: string;
 }
-export default function CustomButton({ title, href, style }: Props) {
+
+export default function CustomButton({
+  title,
+  style,
+  type,
+  clickAction,
+}: Props) {
+  const isOpen = useStore($isOpen);
+  function SwitchClickAction(type: ClickActions) {
+    switch (type) {
+      case ClickActions.OPEN_MODULE:
+        return $isOpen.set(!isOpen);
+      default:
+        break;
+    }
+  }
   return (
-    <button id="custom-button" style={style} className={styles.container}>
-      <a href={href}>
-        <p id="custom-button-title" className={styles.title}>
-          {title}
-        </p>
-      </a>
+    <button
+      type={type}
+      id="custom-button"
+      style={style}
+      className={styles.container}
+      onClick={() => SwitchClickAction(clickAction)}
+    >
+      <p id="custom-button-title" className={styles.title}>
+        {title}
+      </p>
     </button>
   );
 }
