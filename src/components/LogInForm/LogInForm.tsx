798405f5bbd38f65ced113c4a1d07e699bb53838
Sheila Alamo <sheilaalamoalarcon@gmail.com>
2024-11-23 01:08:35 +0000

    fix: login form post was made incorrectly also token was not storage correctly

--- src/components/LogInForm/LogInForm.tsx
+++ src/components/LogInForm/LogInForm.tsx
@@ -1,37 +1,67 @@
-import Input from "../CustomInput";
-import Button from "../CustomButton";
-import { useState } from "preact/hooks";
+import Input from "../Input";
+import Button from "../Button";
+import { useEffect, useState } from "preact/hooks";
 import styles from "./styles.module.css";
-import { ClickActions } from "@/constants/enums";
-import { navigate } from "astro:transitions/client";
+import { API_ROUTES, ClickActions, WebRoutesEnum } from "@/constants/enums";
+import { $isFetching, $token, $userEmail } from "@/constants/constants";
+import Loader from "../Loader";
 
 export default function LoginForm() {
   const [responseMessage, setResponseMessage] = useState("");
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [token, setToken] = useState<string>("");
+
+  useEffect(() => {
+    $isFetching.set(isFetching);
+    localStorage.setItem("isFetching", isFetching.toString());
+  }, [isFetching]);
+
+  useEffect(() => {
+    localStorage.setItem("userEmail", email);
+    $userEmail.set(email);
+  }, [email]);
+
+  useEffect(() => {
+    $token.set(token);
+    localStorage.setItem("token", token);
+  }, [token]);
 
   async function submit(e: SubmitEvent) {
     e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
+    setIsFetching(true);
 
+    const formData = new FormData(e.target as HTMLFormElement);
     const email = formData.get("email");
     const password = formData.get("password");
 
-    const response = await fetch("http://localhost:3600/api/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-      mode: "cors",
-    });
-    const data = await response.json();
-    if (data.message) {
-      setResponseMessage(data.message);
-    }
-    if (response.status === 201) {
-      navigate("/globalChat");
+    try {
+      const response = await fetch(API_ROUTES.LOGIN, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+        mode: "cors",
+      });
+      const data = await response.json();
+
+      if (response.ok) {
+        const token = data.token;
+        setToken(token);
+        setIsFetching(false);
+        setEmail(email?.toString() ?? "");
+        window.location.href = WebRoutesEnum.GLOBAL_CHAT;
+      } else {
+        setIsFetching(false);
+        setResponseMessage(data.message || "Login failed");
+      }
+    } catch (error) {
+      setIsFetching(false);
+      setResponseMessage("An error occurred. Please try again. " + error);
     }
   }
 
@@ -61,6 +91,7 @@ export default function LoginForm() {
       />
 
       {responseMessage && <p>{responseMessage}</p>}
+      <Loader />
     </form>
   );
 }
