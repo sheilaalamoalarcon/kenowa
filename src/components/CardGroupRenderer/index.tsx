7b69a509858ace1b8bb2d92d4e70578f5983ed93
Sheila Alamo <sheilaalamoalarcon@gmail.com>
2024-11-27 01:18:50 +0000

    fix: card group renderer was not working correctly, profile was not as the design

--- src/components/CardGroupRenderer/index.tsx
+++ src/components/CardGroupRenderer/index.tsx
@@ -1,60 +1,40 @@
 import { API_ROUTES } from "@/constants/enums";
 import { useEffect, useState } from "preact/hooks";
-import styles from "./style.module.css";
-import type { CMessage } from "@/constants/classes";
+import { CMessage } from "@/constants/classes";
 import Card from "@/components/Card/index";
+import { Get } from "@/constants/methods";
 
 export default function CardGroupRenderer() {
   const [messages, setMessages] = useState<CMessage[]>([]);
-  const [errors, setErrors] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
   const [token, setToken] = useState<string>("");
-
-  const isEmpty = (value: Array<CMessage>) => value.length === 0;
 
   useEffect(() => {
     setToken(localStorage.getItem("token") ?? "");
   }, [token]);
 
   useEffect(() => {
-    updateMessages();
+    if (token) {
+      Get<CMessage[]>(token, API_ROUTES.GET_ALL_MESSAGES).then((result) => {
+        if (typeof result === "string") {
+          setError(result);
+        } else {
+          setError(null);
+          setMessages(result);
+        }
+      });
+    } else {
+      setError("Could not get ID");
+    }
   }, [token]);
 
-  const updateMessages = async () => {
-    try {
-      const response: Response = await fetch(API_ROUTES.GET_ALL_MESSAGES, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        mode: "cors",
-      });
-
-      if (response.ok) {
-        const data: CMessage[] = await response.json();
-        const isArray = Array.isArray(data);
-        if (isArray && !isEmpty(data)) {
-          setMessages(data);
-          setErrors(null);
-        } else {
-          setMessages([]);
-          setErrors("No messages found: " + data.length);
-        }
-      } else {
-        setErrors("Could not get posts: " + response.statusText);
-      }
-    } catch (error) {
-      const err = error as Error;
-      return setErrors(err.message);
-    }
-  };
-
   return (
-    <div className={styles.container}>
+    <div className={"cardRenderer"}>
+      {error && <p>{error}</p>}
+
       {messages.map((message, index) => {
         return <Card {...message} key={index} />;
       })}
-      {errors && <p>{errors}</p>}
     </div>
   );
 }
