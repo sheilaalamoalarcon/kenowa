151ebc213ab5f8774fa8ed02170d41d07760db93
Sheila Alamo <sheilaalamoalarcon@gmail.com>
2024-11-27 01:40:22 +0000

    fix: alert component was not changing colors correctly

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -6,54 +6,43 @@ import type { IAlert } from "@/constants/classes";
 
 export default function Alert(alert: IAlert) {
   const { type, message } = alert;
-  const [color, setColor] = useState<string>("red");
-  const [cStyle, setcCSyle] = useState<string>(
-    "border-radius:0.4rem;padding:1rem 1rem 1rem 1rem;"
-  );
+  const [color, setColor] = useState<string | null>(null);
 
   const [icon, setIcon] = useState<JSXInternal.Element>(
     SuccessIcon(`var(--${color})`)
   );
-
-  function SwitchByType() {
-    const baseStyle = (color: string) => {
-      return `display: flex;
+  const baseStyle = (color: string) => {
+    return `display: flex;
 flex-direction: row;
 align-items: center;
 gap: 1rem;border-radius:0.4rem;padding:1rem 1rem 1rem 1rem;background:var(--soft-${color}); color: var(--dark-${color});`;
-    };
+  };
 
+  useEffect(() => {
     switch (type) {
       case AlertType.SUCCESS:
         setColor("green");
-        setcCSyle(baseStyle(color));
         setIcon(SuccessIcon(`var(--${color})`));
         return;
       case AlertType.ERROR:
         setColor("red");
-        setcCSyle(baseStyle(color));
         setIcon(ErrorIcon(`var(--${color})`));
 
         return;
       case AlertType.INFO:
         setColor("blue");
-        setcCSyle(baseStyle(color));
         setIcon(InfoIcon(`var(--${color})`));
 
         return;
       default:
         setColor("yellow");
-        setcCSyle(baseStyle(color));
         setIcon(WarningIcon(`var(--${color})`));
 
         return;
     }
-  }
-  useEffect(() => {
-    SwitchByType();
-  }, [type]);
+  }, []);
   return (
-    <div style={cStyle}>
+    <div style={baseStyle(color ?? "")}>
       {icon}
       <p class={"error"}>{message}</p>
     </div>
