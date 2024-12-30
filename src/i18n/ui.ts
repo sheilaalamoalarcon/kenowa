74d8385de473fa1861e6ed1fb0b52876084cc560
Sheila Alamo <sheilaalamoalarcon@gmail.com>
2024-12-30 01:21:12 +0000

    feat: add i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,132 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "nav.logIn": "Log In",
+    "nav.logOut": "Log Out",
+    "nav.chat": "Chat",
+    "nav.profile": "Profile",
+    "nav.create": "Create Post",
+    "copy.welcomeMessage":
+      "Welcome to your go-to space for genuine insights and creative inspiration. Explore trusted reviews, share your own experiences, and find fresh style ideas, all in one place. Dive into our global chat to connect with a vibrant community, exchange opinions, and spark new conversations. Whether you're searching for the best recommendations or looking to inspire others, this is where real voices meet innovative ideas. Join the conversation and start exploring now!",
+    "p.copy.title.1": "Share All Your Visions",
+    "p.copy.subtitle.1": "Connect, Share, and Collaborate",
+    "p.copy.description.1":
+      "Join the conversation in our Global Chat! This space is designed for you to connect with the entire community, exchange ideas, get feedback, and spark new discussions. It’s a real-time feature where you can share your thoughts and explore new perspectives with like-minded users. Dive in, start a chat, and let your voice be part of the collective conversation. This is where ideas come to life — together.",
+
+    "p.copy.title.2": "Search Honest Reviews",
+    "p.copy.subtitle.2": "Find Genuine Insights",
+    "p.copy.description.2":
+      "Discover authentic feedback. Skip the noise and get straight to the real experiences from users like you. Whether you're looking for opinions on products, services, or experiences, our platform prioritizes transparency, helping you make informed decisions with trustworthy, unbiased reviews. Find what matters most with confidence.",
+
+    "p.copy.title.3": "Get Style Inspiration",
+    "p.copy.subtitle.3": "Discover Your Next Look",
+    "p.copy.description.3":
+      "Unleash your creativity. Browse a curated selection of looks, ideas, and trends from real users and fashion enthusiasts. Whether you're updating your wardrobe or looking for fresh styling tips, this is your go-to space for discovering the latest trends and finding inspiration tailored to your taste. Start exploring and elevate your style game today!",
+    "footer.nav": "Project",
+    "footer.nav.contact": "Developer info",
+
+    /* project page*/
+    "project.title": " Kenowa by Sheila Álamo",
+    "project.description.title": "Description",
+    "project.description.strong":
+      " An online platform connecting young and middle-aged women passionate about fashion, allowing them to share styles and opinions within the industry.",
+    "project.description":
+      "Have you ever tried searching for fashion tips or clothing articles, only to get lost in unrelated media? Kenowa aims to be a dedicated platform for environmentally conscious individuals who love or work in the fashion industry. Users can share their opinions, showcase their outfits, and create a digital wardrobe.",
+
+    "input.content": "content",
+    "input.content.placeholder": "Enter your message",
+    "input.image": "image",
+    "input.image.placeholder": "Select an image",
+    "button.sendMessage": "Create Post",
+    "button.startsharing": "start sharing",
+    "profile.post.title": "Your Posts",
+    "profile.saved.title": "Saved",
+
+    "logIn.google": "Log in with Google",
+    "logIn.copy.big":
+      "Whether you're searching for the best recommendations or looking to inspire others, this is where real voices meet innovative ideas. Join the conversation and start exploring now!",
+    "logIn.copy.small.1": "Connect, Share, and Collaborate",
+    "logIn.copy.small.2": "Find Genuine Insights",
+    "logIn.copy.small.3": "Get Style Inspiration",
+
+    "project.develpment.title": "Development Details",
+    "project.develpment.choosenStack": "Choosen Stack",
+    "project.develpment.front": "Astro, TypeScript, CSS, HTML",
+    "project.develpment.back": "Astro, TypeScript, CSS, HTML",
+    "project.develpment.front.title": "Frontend: ",
+    "project.develpment.back.title": "Backend: ",
+    "project.develpment.justification": "Justification",
+    "project.develpment.justification.text":
+      "We chose Astro for the frontend due to its performance benefits and flexibility in building static websites, which is ideal for a content-rich platform. TypeScript enhances code reliability with type safety. For the backend, NodeJS with Express offers a robust and scalable solution, while SQL databases provide efficient data management for user content.",
+    "project.develpment.complications": "Complications",
+    "project.develpment.complications.text":
+      "One of the challenges we faced was ensuring the platform could handle a high volume of user-generated content while maintaining performance. We also needed to implement effective moderation tools to keep the community safe and welcoming.",
+  },
+  es: {
+    "nav.logIn": "Iniciar Sesión",
+    "nav.logOut": "Cerrar Sesión",
+    "nav.chat": "Chat",
+    "nav.profile": "Tu Perfil",
+    "nav.create": "Crear Post",
+    "copy.welcomeMessage":
+      "Bienvenido a tu espacio de referencia para obtener ideas genuinas e inspiración creativa. Explora reseñas confiables, comparte tus propias experiencias y encuentra nuevas ideas de estilo, todo en un solo lugar. Sumérgete en nuestro chat global para conectarte con una comunidad vibrante, intercambiar opiniones y generar nuevas conversaciones. Ya sea que estés buscando las mejores recomendaciones o quieras inspirar a otros, aquí es donde las voces reales se encuentran con ideas innovadoras. ¡Únete a la conversación y comienza a explorar ahora!",
+    "p.copy.title.1": "Comparte Todas Tus Visiones",
+    "p.copy.subtitle.1": "Conéctate, Comparte y Colabora",
+    "p.copy.description.1":
+      "¡Únete a la conversación en nuestro Chat Global! Este espacio está diseñado para que te conectes con toda la comunidad, intercambies ideas, recibas comentarios y generes nuevas discusiones. Es una función en tiempo real donde puedes compartir tus pensamientos y explorar nuevas perspectivas con usuarios afines. Sumérgete, inicia un chat y deja que tu voz sea parte de la conversación colectiva. Aquí es donde las ideas cobran vida, juntos.",
+
+    "p.copy.title.2": "Busca Reseñas Honestas",
+    "p.copy.subtitle.2": "Encuentra Perspectivas Genuinas",
+    "p.copy.description.2":
+      "Descubre comentarios auténticos. Omite el ruido y ve directamente a las experiencias reales de usuarios como tú. Ya sea que busques opiniones sobre productos, servicios o experiencias, nuestra plataforma prioriza la transparencia, ayudándote a tomar decisiones informadas con reseñas confiables e imparciales. Encuentra lo que más importa con confianza.",
+
+    "p.copy.title.3": "Obtén Inspiración de Estilo",
+    "p.copy.subtitle.3": "Descubre Tu Próximo Look",
+    "p.copy.description.3":
+      "Desata tu creatividad. Explora una selección curada de looks, ideas y tendencias de usuarios reales y entusiastas de la moda. Ya sea que estés actualizando tu guardarropa o buscando nuevos consejos de estilo, este es tu espacio de referencia para descubrir las últimas tendencias y encontrar inspiración adaptada a tu gusto. ¡Comienza a explorar y eleva tu estilo hoy mismo!",
+    "footer.nav": "Proyecto",
+    "footer.nav.contact": "Información del desarrollador",
+
+    /* project page*/
+    "project.title": " Kenowa by Sheila Álamo",
+    "project.description.title": "Descripción",
+    "project.description.strong":
+      "Una plataforma online que conecta a mujeres jóvenes y de mediana edad apasionadas por la moda, permitiéndoles compartir estilos y opiniones dentro de la industria.",
+    "project.description":
+      "¿Alguna vez has intentado buscar consejos de moda o artículos de ropa, solo para perderte en medios no relacionados? Kenowa pretende ser una plataforma dedicada a personas conscientes del medio ambiente que aman o trabajan en la industria de la moda. Los usuarios pueden compartir sus opiniones y mostrar sus conjuntos. y crear un guardarropa digital.",
+
+    "input.content": "contenido",
+    "input.content.placeholder": "Enter your message",
+    "input.image": "imagen",
+    "input.image.placeholder": "Selecciona una imagen",
+    "button.sendMessage": "Crear Publicación",
+    "button.startsharing": "empieza a compartir",
+    "profile.post.title": "Tus Publicaciones",
+    "profile.saved.title": "Guardados",
+    "logIn.google": "Log in con Google",
+    "logIn.copy.big":
+      "Ya sea que esté buscando las mejores recomendaciones o buscando inspirar a otros, aquí es donde las voces reales se encuentran con ideas innovadoras. ¡Únase a la conversación y comience a explorar ahora!",
+    "logIn.copy.small.1": "Conéctate, comparte y colabora",
+    "logIn.copy.small.2": "Encuentre información genuina",
+    "logIn.copy.small.3": "Obtén inspiración sobre estilo",
+
+    "project.develpment.title": "Development Details",
+    "project.develpment.choosenStack": "Choosen Stack",
+    "project.develpment.front": "Astro, TypeScript, CSS, HTML",
+    "project.develpment.back": "Astro, TypeScript, CSS, HTML",
+    "project.develpment.front.title": "Frontend: ",
+    "project.develpment.back.title": "Backend: ",
+    "project.develpment.justification": "Justificación",
+    "project.develpment.justification.text":
+      "Elegimos Astro para el frontend debido a sus beneficios de rendimiento y flexibilidad en la creación de sitios web estáticos, lo cual es ideal para una plataforma rica en contenido. TypeScript mejora la confiabilidad del código con seguridad de tipos. Para el backend, NodeJS con Express ofrece una solución robusta y escalable , mientras que las bases de datos SQL proporcionan una gestión eficiente de los datos del contenido del usuario.",
+    "project.develpment.complications": "Complicaciones",
+    "project.develpment.complications.text":
+      "Uno de los desafíos que enfrentamos fue garantizar que la plataforma pudiera manejar un gran volumen de contenido generado por el usuario mientras mantenía el rendimiento. También necesitábamos implementar herramientas de moderación efectivas para mantener a la comunidad segura y acogedora",
+  },
+} as const;
